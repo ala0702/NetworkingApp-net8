bb282b0406a9d4ef1cb4e1e5528eb9fcee70e59e
ala0702 <ala07022019@gmail.com>
2024-07-20 17:04:00 +0200

    feat: API skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using API.Controllers;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        var users = await context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id){
+
+        var user = await context.Users.FindAsync(id);
+        if(user != null){
+            return Ok(user);
+        }
+        else{
+            return NotFound();
+        }
+    }
+}
