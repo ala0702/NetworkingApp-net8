9ec1b4e80d63f901498d1057b44d00f68468dad2
ala0702 <ala07022019@gmail.com>
2024-07-21 12:00:01 +0200

    feat: client skeleton

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  https = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void {
+    this.https.get('https://localhost:5001/users').subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Request complited');
+      },
+    });
+  }
+}
